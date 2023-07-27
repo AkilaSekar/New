d2f5d262dd7d4f71295fb9272c0714b0154e072e
Akila <akilasekar1997@gmail.com>
2023-07-27 15:38:35 +0530

    commit

---
+++ Day 18/promiseall.js
@@ -0,0 +1,51 @@
+let promiseObj=new Promise((resolve,reject)=>{
+    console.log("Promise Object invoked");
+    // resolve("Success");
+    reject("Failure");
+});
+console.log(promiseObj);
+promiseObj
+.then((val)=>{
+    console.log("success",val);
+})
+.catch((err)=>{
+    console.log("fail",err);
+})
+
+
+function function1(){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            resolve("Task 1 is completed!!!");
+        },1000);
+    })
+}
+
+function function2(){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            resolve("Task 2 is completed!!!");
+        },1000);
+    })
+}
+
+function function3(){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            reject("Task 3 is not completed!!!");
+        },1000);
+    })
+}
+
+
+Promise.all([function1(),function2(),function3()])
+.then((val)=>{
+    console.log(val);
+    console.log(" All the tasks are completed!!!");
+
+})
+.catch((err)=>{
+    console.log("Error:",err);
+})
+
+
