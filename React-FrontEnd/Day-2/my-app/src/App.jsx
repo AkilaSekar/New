a32bf23243ab16eb3b9bfbc0410e0defcb0ddbdd
Akila <akilasekar1997@gmail.com>
2023-08-08 22:35:13 +0530

    commit

---
+++ React-FrontEnd/Day-2/my-app/src/App.jsx
@@ -0,0 +1,15 @@
+
+
+function App() {
+  
+  return (
+    <>
+      
+      <h1>Hello Akila Sekar</h1>
+      <h1>Testing </h1>
+      <p>I am learning React Js</p>
+    </>
+  )
+}
+
+export default App
